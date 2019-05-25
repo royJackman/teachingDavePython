136b72ca090ff3a43941da1543bb0eb769792ba6
Roy <jackman.roy@gmail.com>
2019-05-25 19:33:42 -0400

    Adding warmup

---
+++ WarmUpFile.py
@@ -0,0 +1,26 @@
+# Print the sum of a and b
+
+def addition(a, b):
+    pass
+
+# Print all of the numbers from 0 to x, inclusive. 
+# One per line
+
+def print_all_leq_x(x):
+    pass
+
+# Sort the list to be in ascending order
+
+def sort_list(lst):
+    pass
+
+# Print a square grid of numbers that cascades from x outwards
+# For example: print_grid(3) should print
+# 11111
+# 12221
+# 12321
+# 12221
+# 11111
+
+def print_grid(x):
+    pass
